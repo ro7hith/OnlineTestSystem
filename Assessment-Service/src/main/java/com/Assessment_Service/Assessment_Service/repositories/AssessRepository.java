28fa466e2db6a5c0f199ba6a9085ab65d65bef8c
ro7hith <rohithkadaru@gmail.com>
2025-04-17 10:42:02 -0500

    Changes

--- Assessment-Service/src/main/java/com/Assessment_Service/Assessment_Service/repositories/AssessRepository.java
+++ Assessment-Service/src/main/java/com/Assessment_Service/Assessment_Service/repositories/AssessRepository.java
@@ -16,4 +16,21 @@ public interface AssessRepository extends JpaRepository<Assessment, Integer> {
             "WHERE A.assessment_id = :assessmentId",
     nativeQuery = true)
 		List<Object[]> findAssessmentDetailsById(@Param("assessmentId") int assessmentId);
+		
+		@Query(value = """
+			    SELECT a.email, 
+			           a.technology, 
+			           a.question_level, 
+			           ROUND(AVG(CASE 
+			                        WHEN s.answer = s.selected_option THEN 1 
+			                        ELSE 0 
+			                    END) * 100, 2) AS average_score
+			    FROM assessment a
+			    JOIN assess_score s ON a.assessemnt_id = s.assessment_id
+			    WHERE a.email = :email
+			    GROUP BY a.email, a.technology, a.question_level
+			    ORDER BY a.technology, a.question_level
+			    """, nativeQuery = true)
+			List<Object[]> fetchUserTopicAveragesByEmail(@Param("email") String email);
+
 }
